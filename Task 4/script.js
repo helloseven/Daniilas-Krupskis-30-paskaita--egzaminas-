84a9b1ffc09aaec73330e6f29ee52c4a39638422
DANIILAS KRUPSKIS <7hello@gmail.com>
2022-01-12 20:32:31 +0200

    Pridėtas stilius

--- Task 4/script.js
+++ Task 4/script.js
@@ -16,11 +16,19 @@ fetch(ENDPOINT)
 .then(data => {
     data.forEach(element => {
         let output = document.querySelector('#output');
+        // Create a card
+        let card = document.createElement('div');
+        card.setAttribute('id', 'card');
+        // Create brand name
         let brand = document.createElement('h1');
         brand.textContent = element.brand;
+        // Create model list
         let models = document.createElement('h4');
         models.textContent = element.models;
-        output.append(brand, models);
+        // Append to card
+        card.append(brand, models);
+        // Append to output
+        output.append(card);
         
     });
 })
