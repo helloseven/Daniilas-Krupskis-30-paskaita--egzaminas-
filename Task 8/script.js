d8c8d0e06364561c7f07231ea027a9456bf23fd4
DANIILAS KRUPSKIS <7hello@gmail.com>
2022-01-13 19:07:00 +0200

    Pridėta konstruktoriaus funkcija pagal užduotį

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,26 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą.
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function Calculator(num1, num2){
+        this.num1 = num1;
+        this.num2 = num2;
+        this.sum = function(){
+            return this.num1 + this.num2;
+        }
+        this.substraction = function(){
+            return this.num1 - this.num2;
+        }
+        this.multiplication = function(){
+            return this.num1 * this.num2;
+        }
+        this.division = function(){
+            return this.num1 / this.num2;
+        }
+}
+
+let newCalc = new Calculator(2,2);
+console.log(newCalc.sum());
+console.log(newCalc.substraction());
+console.log(newCalc.multiplication());
+console.log(newCalc.division());
